524a2ae65cdd20fc97af649466349542d1c7449d
niocio01 <zuber.nico@gmail.com>
2020-08-05 00:45:06 +0200

    added screen dimming and button Matrix

--- include/display.h
+++ include/display.h
@@ -6,9 +6,11 @@
 
 #define dispWidth 240
 #define dispHeight 135
+#define DEFAULT_SCREEN_BRIGHTNESS 100
 
 void guiTask(void *pvParameter);
 void display_init(void);
+void setScreenBrightness(uint8_t brightness);
 
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p);
 bool read_encoder(lv_indev_drv_t * indev, lv_indev_data_t * data);
--- src/display.cpp
+++ src/display.cpp
@@ -10,6 +10,7 @@
 #include "freertos/semphr.h"
 #include "esp_system.h"
 #include "driver/gpio.h"
+#include "arduino.h"
 
 /* Littlevgl specific */
 #include <lvgl.h>
@@ -28,10 +29,14 @@ lv_obj_t *test2;
 
 void guiTask(void *pvParameter)
 {
+    
+
     while (1)
     {
         uint32_t waitTime = lv_task_handler(); /* let the GUI do its work */
         vTaskDelay(waitTime / portTICK_PERIOD_MS);
+
+     
     }
 
     //A task should NEVER return
@@ -40,6 +45,7 @@ void guiTask(void *pvParameter)
 
 void toggle(void *pvParameter)
 {
+
     while (1)
     {
         vTaskDelay(500/ portTICK_PERIOD_MS);
@@ -58,6 +64,10 @@ void display_init(void)
 
     tft.begin(); /* TFT init */
     tft.setRotation(1); /* Landscape orientation */
+
+    ledcSetup(0, 4000, 8); // setup PWM for Backlight of Screen
+    ledcAttachPin(4, 0);
+    setScreenBrightness(DEFAULT_SCREEN_BRIGHTNESS);
 
     lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * 10);
 
@@ -79,35 +89,38 @@ void display_init(void)
 
     /* Create simple label */
     lv_obj_t *label = lv_label_create(lv_scr_act(), NULL);
-    lv_label_set_text(label, "Filament Box - No Programm Running");
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SROLL);
-    lv_label_set_anim_speed(label, 15);
+    lv_label_set_text(label, "No programm running  -  Select a preset or create a new one  -");
+    lv_label_set_long_mode(label, LV_LABEL_LONG_SROLL_CIRC);
+    lv_label_set_anim_speed(label, 20);
     lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_LEFT, 5, 5);
     lv_obj_set_width(label, dispWidth - 10);
 
-    // test = lv_switch_create(lv_scr_act(), NULL);
-    // lv_obj_align(test, label, LV_ALIGN_OUT_BOTTOM_LEFT, 5, 5);
-    // test2 = lv_switch_create(lv_scr_act(), NULL);
-    // lv_obj_align(test2, test, LV_ALIGN_OUT_RIGHT_MID, 5, 0);
-    // lv_switch_on(test2, LV_ANIM_OFF);
+    test = lv_switch_create(lv_scr_act(), NULL);
+    lv_obj_align(test, label, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 5);
+    test2 = lv_switch_create(lv_scr_act(), NULL);
+    lv_obj_align(test2, test, LV_ALIGN_OUT_RIGHT_MID, 5, 0);
+    lv_switch_on(test2, LV_ANIM_OFF);
 
     
-    static const char* btnm_map[] = {"PLA", "PETG", "NEW", ""};
+    static const char* btnm_map[] = {"PLA", "ABS", "PETG", "\n", "TPU-R", "PC", "+", ""};
     lv_obj_t *buttons = lv_btnmatrix_create(lv_scr_act(), NULL);
     lv_btnmatrix_set_map(buttons, btnm_map);
 
-    lv_obj_set_size(buttons, dispWidth-10, 80);
+    lv_obj_set_size(buttons, dispWidth-10, 70);
     // lv_btnmatrix_set_btn_ctrl_all(buttons, LV_BTNMATRIX_CTRL_DISABLED);
-    lv_obj_align(buttons, label, LV_ALIGN_OUT_BOTTOM_MID, 0, 5);
+    lv_obj_align(buttons, test, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 5);
     lv_btnmatrix_set_focused_btn(buttons, 0);
-
-    
 
 
     xTaskCreate(guiTask, "GuiTask", 1025 * 4, NULL, 1, NULL);
-    //xTaskCreate(toggle, "toggleTask", 1025 * 4, NULL, 1, NULL);
+    xTaskCreate(toggle, "toggleTask", 1025 * 4, NULL, 1, NULL);
 }
 
+
+void setScreenBrightness(uint8_t brightness)
+{
+    ledcWrite(0, brightness);
+}
 
 #if LV_USE_LOG != 0
 /* Serial debugging */
